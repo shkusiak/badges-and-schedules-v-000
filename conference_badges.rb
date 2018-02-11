ccc3947eeb9a891bde993cf21a7d76f0eb4788a8
Shea Kusiak <shkusiak@gmail.com>
2018-02-11 17:31:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,11 +22,11 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  
+
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  
+
   assign_rooms(attendees).each do |assignment|
     puts assignment
   end
